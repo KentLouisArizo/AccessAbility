d76fe6295ea9c6fb38b0f42aec51ec2de0962950
KentLouisArizo <111263205+KentLouisArizo@users.noreply.github.com>
2024-11-26 00:35:08 +0000

    styles1

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,46 +12,54 @@ const Homepage = () => {
 
   return (
     <div className="homepage-container">
-      <main className="homepage-main">
-        <div className="content-container">
-          <div className="content-grid">
-            <div className="content-text">
-              <h1 className="title">Welcome to PWD & PDAO</h1>
-              <p className="description">
-                This website is designed to serve the needs of Persons with Disabilities (PWDs) and Persons with
-                Disability Affairs Offices (PDAOs) in our community. We strive to provide resources, information, and
-                support to empower and assist those with disabilities.
-              </p>
-              <div className="button-group">
-                <div className="dropdown">
-                  <button
-                    className="button-primary"
-                    onClick={toggleDropdown}
-                  >
-                    Register
-                  </button>
-                  {dropdownVisible && (
-                    <div className="dropdown-content">
-                      <Link to="/register-PWD" className="dropdown-item">
-                        Register PWD
-                      </Link>
-                      <Link to="/register-Relative" className="dropdown-item">
-                        Register Relative
-                      </Link>
-                    </div>
-                  )}
+      {/* Left Section */}
+      <div className="logo-section">
+        <div className="logo-container">
+          <img src={PDAOlogo} alt="Large Logo" className="large-logo" />
+          <img src={PDAOlogo} alt="Main Logo" className="logo-image" />
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="info-section">
+        <div className="info-content">
+          <h1 className="title">AccessAbility</h1>
+          <p className="description">
+          This website is designed to serve the needs of Persons with 
+          Disabilities (PWDs) and Persons with Disability Affairs Offices 
+          (PDAOs) in our community. We strive to provide resources, 
+          information, and support to empower and assist those with 
+          disabilities.
+          </p>
+
+          {/* Buttons */}
+          <div className="button-group">
+            <Link to="/login" className="button-secondary">
+              Login
+            </Link>
+            <div className="dropdown">
+              <button className="button-primary" onClick={toggleDropdown}>
+                Register
+              </button>
+              {dropdownVisible && (
+                <div className="dropdown-content">
+                  <Link to="/register-PWD" className="dropdown-item">
+                    Register PWD
+                  </Link>
+                  <Link to="/register-Relative" className="dropdown-item">
+                    Register Relative
+                  </Link>
                 </div>
-                <Link to="/login" className="button-secondary">
-                  Login
-                </Link>
-              </div>
-            </div>
-            <div className="content-image">
-              <img src={PDAOlogo} alt="PWD & PDAO" className="image" />
+              )}
             </div>
           </div>
+
+          {/* Forgot Password */}
+          <Link to="/reset-password" className="forgot-password">
+            Forgot Password
+          </Link>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
